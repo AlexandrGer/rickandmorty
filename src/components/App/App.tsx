5f952dc98c5c424b9fb9594a8bb0770fa6f36492
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-10-14 19:10:46 +0300

    Функции поиска

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import Header from "../Header/Header";
 import "./App.css";
 import Character from "../Character/Character";
@@ -7,32 +7,62 @@ import SearchForm from "../SearcForm/SearchForm";
 
 function App() {
   const [characters, setCharacters] = useState<ICharacter[]>([]);
+  const [episode, setEpisode] = useState<any[]>([]);
 
-  async function getCharacters() {
-    try {
-      const response = await fetch(
-        "https://rickandmortyapi.com/api/character/?page=42"
-      );
-      return response.json().then((res) => setCharacters(res.results));
-    } catch (err) {
-      console.log(err);
+  const getCharacters = useCallback(async (searchQuery: any) => {
+    const params = new URLSearchParams(searchQuery).toString();
+    if (searchQuery.episode !== "") {
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/episode/?${params}`
+        );
+        setCharacters([]);
+        return response.json().then((res) => setEpisode(res.results));
+      } catch (err) {
+        setEpisode([]);
+        console.log(err);
+      }
+    } else {
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character/?${params}`
+        );
+        setEpisode([]);
+        return response.json().then((res) => setCharacters(res.results));
+      } catch (err) {
+        setCharacters([]);
+        console.log(err);
+      }
     }
-  }
-
-  useEffect(() => {
-    getCharacters();
   }, []);
+
   console.log(characters);
+  console.log(episode);
+
   return (
     <div className="bg-black max-w-[120.00rem] h-full py-5 px-2">
       <Header />
-      <SearchForm />
+      <SearchForm getCharacters={getCharacters} />
       <ul className="flex flex-col items-center gap-5">
-        {characters.map((character) => (
-          <Character key={character.id} character={character} />
-        ))}
+        {episode && episode.length > 0 ? (
+          <>
+            {episode.map((character) => (
+              <Character key={character.id} character={character} />
+            ))}
+          </>
+        ) : (
+          <>
+            {characters &&
+              characters.map((character) => (
+                <Character key={character.id} character={character} />
+              ))}
+          </>
+        )}
+        {/* {characters &&
+          characters.map((character) => (
+            <Character key={character.id} character={character} />
+          ))} */}
       </ul>
-      {/* </div> */}
     </div>
   );
 }
--- src/components/SearcForm/SearchForm.tsx
+++ src/components/SearcForm/SearchForm.tsx
@@ -1,14 +1,65 @@
-export default function SearchForm() {
+import React, { useState, useEffect, useMemo } from "react";
+
+type Props = {
+  getCharacters: any;
+};
+
+export default function SearchForm({ getCharacters }: Props) {
+  const [nameInput, setNameInput] = useState<string>(
+    localStorage.getItem("name") || ""
+  );
+  const [statusInput, setStatusInput] = useState<string>(
+    localStorage.getItem("status") || ""
+  );
+  const [speciesInput, setSpeciesInput] = useState<string>(
+    localStorage.getItem("species") || ""
+  );
+  const [episodeInput, setEpisodeInput] = useState<string>(
+    localStorage.getItem("episode") || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("name", nameInput);
+    localStorage.setItem("status", statusInput);
+    localStorage.setItem("species", speciesInput);
+    localStorage.setItem("episode", episodeInput);
+  }, [nameInput, speciesInput, statusInput, episodeInput]);
+
+  const searchQuery = useMemo(
+    () => ({
+      name: nameInput,
+      status: statusInput,
+      species: speciesInput,
+      episode: episodeInput,
+      page: 1,
+    }),
+    [episodeInput, nameInput, speciesInput, statusInput]
+  );
+
+  useEffect(() => {
+    getCharacters(searchQuery);
+  }, [searchQuery]);
+
   return (
     <form className="text-white flex flex-col gap-5 pb-8">
       <div className="flex flex-col gap-[0.31rem]">
         <label>Имя</label>
-        <input type="text" className="input " />
+        <input
+          value={nameInput}
+          type="text"
+          className="input"
+          onChange={(event) => setNameInput(event.target.value)}
+        />
       </div>
       <div className="w-full flex gap-5 ">
         <div className="flex flex-col flex-1 gap-[0.31rem]">
           <label>Жив?</label>
-          <select className="input cursor-pointer">
+          <select
+            className="input cursor-pointer"
+            value={statusInput}
+            onChange={(event) => setStatusInput(event.target.value)}
+          >
+            <option value="">Status</option>
             <option value="Dead">Dead</option>
             <option value="Alive">Alive</option>
             <option value="unknown">Unknown</option>
@@ -16,7 +67,12 @@ export default function SearchForm() {
         </div>
         <div className="flex flex-col flex-1 gap-[0.31rem]">
           <label>Раса</label>
-          <select className="input cursor-pointer">
+          <select
+            className="input cursor-pointer"
+            value={speciesInput}
+            onChange={(event) => setSpeciesInput(event.target.value)}
+          >
+            <option value="">Species</option>
             <option value="Human">Human</option>
             <option value="Alien">Alien</option>
             <option value="Humanoid">Humanoid</option>
@@ -26,7 +82,13 @@ export default function SearchForm() {
       </div>
       <div className="flex flex-col gap-[0.31rem]">
         <label>Эпизод</label>
-        <input type="number" className="input " />
+        <input
+          type="number"
+          className="input"
+          value={episodeInput}
+          min={1}
+          onChange={(event) => setEpisodeInput(event.target.value)}
+        />
       </div>
     </form>
   );
